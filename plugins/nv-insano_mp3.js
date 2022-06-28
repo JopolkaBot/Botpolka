3ace96191bf4f39ec7e079c74bb07bf58988565d
JopolkaBot <108308949+JopolkaBot@users.noreply.github.com>
2022-06-27 22:44:22 -0500

    Create nv-insano_mp3.js

---
+++ plugins/nv-insano_mp3.js
@@ -0,0 +1,16 @@
+import util from 'util'
+import path from 'path'
+
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = './media/insano.mp3.mp3'
+conn.sendFile(m.chat, vn, 'insano.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /insano|inzanp|insanidad|friGOD|frifayer/
+handler.command = new RegExp
+export default handler
+
+
