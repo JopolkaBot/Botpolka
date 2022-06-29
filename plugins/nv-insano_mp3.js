ceb5d34e00913b5084342d66a1c428b15f0ce9f9
JopolkaBot <108308949+JopolkaBot@users.noreply.github.com>
2022-06-29 12:13:24 -0500

    Update nv-insano_mp3.js

--- plugins/nv-insano_mp3.js
+++ plugins/nv-insano_mp3.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = '.media/insano.mp3'
+let vn = './media/insano.mp3'
 conn.sendFile(m.chat, vn, 'insano.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
