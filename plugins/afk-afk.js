dc4223890e056cf7222f1554dcefb6ec52105f9e
JopolkaBot <108308949+JopolkaBot@users.noreply.github.com>
2022-07-01 20:41:05 -0500

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,7 +2,7 @@ let handler = async (m, { text }) => {
 let user = global.db.data.users[m.sender]
 user.afk = + new Date
 user.afkReason = text
-m.reply(`*[❗𝐈𝐍𝐅𝐎❗] activaste el crasheo ${conn.getName(m.sender)} necesitamos otro insano sin miedo*\n\n*—◉ )${text ? ': ' + text : ''}*
+m.reply(`*[❗𝐈𝐍𝐅𝐎❗] activaste el crasheo ${conn.getName(m.sender)} necesitamos otro insano sin miedo*\n\n* )${text ? ': ' + text : ''}*
 `)}
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
