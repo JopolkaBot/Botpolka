8d18f8c7f4f1f3b330c27a5474d23ef52c1c9429
Jopolka <108308949+JopolkaBot@users.noreply.github.com>
2022-07-05 01:57:58 -0500

    Create brayan-pack.js

---
+++ plugins/brayan-pack.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+let url = uriel[Math.floor(Math.random() * uriel.length)]
+conn.sendButton(m.chat, `_Brayan Pérez_`, author, url, [['🛐 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🛐', `/${command}`]], m)
+}
+handler.help = ['Brayan']
+handler.tags = ['internet']
+handler.command = /^(Bryan)$/i
+export default handler
+
+global.bryan = [
+  "https://telegra.ph/file/71da5381c26c46ba17263.jpg",
+  "https://telegra.ph/file/41fb81c0e60fb73ed7425.jpg",
+  "https://telegra.ph/file/e5a0a4f1b68158e9d9e49.jpg",
+   ]
