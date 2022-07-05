c120c86db738195a4551477797c0e41e49a2e1c9
Jopolka <108308949+JopolkaBot@users.noreply.github.com>
2022-07-05 18:27:43 -0500

    Delete sermi.js

--- sermi.js
+++
@@ -1,7 +0,0 @@
-const pp = './media/sermino.jpg'
-let text = `acepta ♡`
-handler.help = ['novia']
-handler.tags = ['novi']
-handler.command = /^(Novia?|novia?|chava?|Chava)
-handler.group = true
-export default handler
