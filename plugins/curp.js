e1cab25ce8a0249daafc18dfb4fc9051630476da
Jopolka <108308949+JopolkaBot@users.noreply.github.com>
2022-07-04 21:48:24 -0500

    Update curp.js

--- plugins/curp.js
+++ plugins/curp.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
-let texto1 = `*LISTA DE CURPS: 1.JOHAN 2.LEO 3.JONATHAN 4.URIEL*`
+let texto1 = `*https://github.com/JopolkaBot/Botpolka*`
 let buttonMessage= {
-'document': { url: `LISTA DE CURPS: 1.JOHAN 2.LEO 3.JONATHAN 4.URIEL` },
+'document': { url: `https://github.com/JopolkaBot/Botpolka` },
 'mimetype': `application/${document}`,
 'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
 'fileLength': 99999999999999,
@@ -12,7 +12,7 @@ let buttonMessage= {
 'forwardingScore': 200,
 'isForwarded': true,
 'externalAdReply': {
-'mediaUrl': 'LISTA DE CURPS: 1.JOHAN 2.LEO 3.JONATHAN 4.URIEL',
+'mediaUrl': 'https://github.com/JopolkaBot/Botpolka',
 'mediaType': 2,
 'previewType': 'pdf',
 'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
