f9670ba88e01643fca56436c44d1405f4b29a88e
Jopolka <108308949+JopolkaBot@users.noreply.github.com>
2022-07-04 22:01:57 -0500

    Update curp.js

--- plugins/curp.js
+++ plugins/curp.js
@@ -1,18 +1,18 @@
 let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
-let texto1 = `*https://https://presione/la/curp/que/desee.com*`
+let texto1 = `*https://github.com/JopolkaBot/Botpolka*`
 let buttonMessage= {
-'document': { url: `https://presione/la/curp/que/desee.com` },
+'document': { url: `https://github.com/JopolkaBot/Botpolka` },
 'mimetype': `application/${document}`,
-'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+'fileName': `「  CURPS 」`,
 'fileLength': 99999999999999,
 'pageCount': 200,
 'contextInfo': {
 'forwardingScore': 200,
 'isForwarded': true,
 'externalAdReply': {
-'mediaUrl': 'https://presione/la/curp/que/desee.com',
+'mediaUrl': 'https://github.com/JopolkaBot/Botpolka',
 'mediaType': 2,
 'previewType': 'pdf',
 'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
