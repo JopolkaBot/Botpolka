c3abc6094903e8f152f2ec26d1093c526e0c6b92
Jopolka <108308949+JopolkaBot@users.noreply.github.com>
2022-07-05 18:06:11 -0500

    Create sermi.js

---
+++ sermi.js
@@ -0,0 +1,8 @@
+let handler = async (m, { conn, participants, groupMetadata }) => {
+const pp = './sermino.jpg'
+let text = `acepta ♡`
+handler.help = ['novia']
+handler.tags = ['novi']
+handler.command = /^(Novia?|novia?|chava?|Chava))$/i
+handler.group = true
+export default handler
