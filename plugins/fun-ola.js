10f0fde325abefb64216e90bfeeaf54759f75a6c
Jopolka <108308949+JopolkaBot@users.noreply.github.com>
2022-07-06 21:56:31 -0500

    Update fun-ola.js

--- plugins/fun-ola.js
+++ plugins/fun-ola.js
@@ -1,3 +1,5 @@
+import util from 'util'
+import path from 'path'
 let handler = async (m, { conn, command, text }) => {
 let text = `*Eh hola 😅 aña 😼, perdón por encarar Messi 😓, es que te vi 😍 y dije 🙈 ig de la minita 😝, queremos la Champions 👻, soy un lobo solitario 😼 en busca de una loba 🐺🐺 Auuuuuuuu 🐺👻😼 soy heroico en fri fayer 👻 y los dejo 7-0 a todos 😝 GUASAAA 👻📞, quieres ser mi bebita fui fui?🙈 Es que te veo🤩 y digo😳, jijijija 😜 pasa pack 👻*
 `.trim(), m, m.mentionedJid ? {
