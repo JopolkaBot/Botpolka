c9cc7c509917794d8d029233145281280580744e
Jopolka <108308949+JopolkaBot@users.noreply.github.com>
2022-07-06 20:34:48 -0500

    Update nv-hola.js

--- plugins/nv-hola.js
+++ plugins/nv-hola.js
@@ -2,10 +2,9 @@ import util from 'util'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let text = 'Eh hola 😅 aña 😼, perdón por encarar Messi 😓, es que te vi 😍 y dije 🙈 ig de la minita 😝, queremos la Champions 👻, soy un lobo solitario 😼 en busca de una loba 🐺🐺 Auuuuuuuu 🐺👻😼 soy heroico en fri fayer 👻 y los dejo 7-0 a todos 😝 GUASAAA 👻📞, quieres ser mi bebita fui fui?🙈 Es que te veo🤩 y digo😳, jijijija 😜 pasa pack 👻'
 conn.text(m.chat, vn, 'Eh hola 😅 aña 😼, perdón por encarar Messi 😓, es que te vi 😍 y dije 🙈 ig de la minita 😝, queremos la Champions 👻, soy un lobo solitario 😼 en busca de una loba 🐺🐺 Auuuuuuuu 🐺👻😼 soy heroico en fri fayer 👻 y los dejo 7-0 a todos 😝 GUASAAA 👻📞, quieres ser mi bebita fui fui?🙈 Es que te veo🤩 y digo😳, jijijija 😜 pasa pack 👻', null, m, true, {
-type: 'text', 
+type: 'Message', 
 ptt: true 
 })
 }
