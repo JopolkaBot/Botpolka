a6e1a48d383f4a2309c1a8fd4d5955fccae0fe9f
Jopolka <108308949+JopolkaBot@users.noreply.github.com>
2022-07-06 21:47:49 -0500

    Create fun-ola.js

---
+++ plugins/fun-ola.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, command, text }) => {
+conn.reply(m.chat, `
+*Eh hola 😅 aña 😼, perdón por encarar Messi 😓, es que te vi 😍 y dije 🙈 ig de la minita 😝, queremos la Champions 👻, soy un lobo solitario 😼 en busca de una loba 🐺🐺 Auuuuuuuu 🐺👻😼 soy heroico en fri fayer 👻 y los dejo 7-0 a todos 😝 GUASAAA 👻📞, quieres ser mi bebita fui fui?🙈 Es que te veo🤩 y digo😳, jijijija 😜 pasa pack 👻*
+`.trim(), m, m.mentionedJid ? {
+contextInfo: {
+mentionedJid: m.mentionedJid
+}} : {})}
+handler.help = ['hola']
+handler.tags = ['fun']
+handler.customPrefix = /hola|Hola|ola/
+handler.command = /^(Hola|hola|ola?$)/
+export default handler
